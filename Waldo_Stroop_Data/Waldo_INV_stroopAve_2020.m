2d7e5ab39ca4301c4ec244c54875d101c8454743
Penny Shuyi Chen <schen104@u.rochester.edu>
2020-04-02 00:47:56 -0400

    Individual analysis for Inverted-stroop task

---
+++ Waldo_Stroop_Data/Waldo_INV_stroopAve_2020.m
@@ -0,0 +1,102 @@
+%% Waldo Stroop effect Individual aves - PSC 2020 
+% Get individual avearages
+% Conditions: 0-> incongruent; 1-> congruent; 2-> front-view (baseline)
+% 
+clear all; close all; clc;
+
+%% Choose avialable subject
+whichsub = 1;
+remove_outliers= 3; % 0: no / other numbers: criterion (remove_outliers*SD)
+
+%dataset = load(['St' num2str(whichsub) '_waldo_data.mat']);
+dataset = load(['LX_INV_waldo_data.mat']);
+dataset = dataset.data_cell;
+
+% columns: [1trial 2coherence 3ndist 4RT 5meanD2target 6correct]
+data = cell2mat(dataset(:,1:8));
+
+% column id - Stroop effect
+id_trial = 1;
+id_cong = 2;
+id_dirG = 3;
+id_imageGaze = 4;
+id_dirL = 5; %% the perceived gaze direction 
+id_RT = 6;
+id_correct = 7;
+id_block = 8;
+
+% conditions- Stroop 
+
+congruence = [0 1 2];
+
+% add block info
+%nblock = 5;
+%ntrials = size(data,1);
+%ntrblock = ntrials/nblock;
+%temp = [];
+%for i = 1:nblock
+%    temp = [temp; ones(ntrblock,1)*i];
+%end
+%data(:,7) = temp;
+
+% select blocks bigger than 1 
+%checkdata= data(data(:,id_block)==1,:);
+%data = data(data(:,id_block)>1,:);
+
+q = 1;
+for whichcong = congruence
+    
+    % select data in a coherence condition 
+    tempdata1 = data(data(:,id_cong)==whichcong,:);
+
+        % select data in an n distractor condition 
+        %tempdata2 = tempdata1(tempdata1(:,id_ndist)==whichndist,:);
+        
+        % select only the correct trials
+        tempdata2 = tempdata1(tempdata1(:,id_correct)==1,:);
+        
+        % tempSD: calculated based on all RTs for all conditions 
+        meandata = mean(tempdata2);
+        SDdata = std(tempdata2);
+        
+        tempmean = meandata(1,id_RT);
+        tempSD = SDdata(1,id_RT);
+        
+        % outlier removal
+        if remove_outliers > 0 
+            % check 
+            %tempdata4 = tempdata2(abs(tempdata2(:,id_RT)) >= tempmean+remove_outliers*tempSD,:);
+            tempdata3 = tempdata2(abs(tempdata2(:,id_RT)) < tempmean+remove_outliers*tempSD,:);
+        end
+        
+        % recalculate the mean 
+        meandata_removed = mean(tempdata3);
+        std_removed = std(tempdata3);
+  
+        % save
+        meanRT(1,q) = meandata_removed(1,id_RT);
+        std_RT(1,q) = std_removed(1,id_RT);
+        meanAccuracy(1,q) = length(tempdata2)/100;
+        q = q+1;
+
+end 
+
+%% check 
+%right_count=data(data(:,id_correct)==1,id_correct);
+%accuracy = sum(right_count)/length(data);
+figure(1);
+%for i = 1:3
+subplot(2,1,1);
+plot(congruence,meanRT(1,:)); hold on;
+errorbar(congruence,meanRT(1,:),std_RT(1,:));
+xlabel('Congruence');
+title('Individual RT');
+
+subplot(2,1,2);
+plot(congruence,meanAccuracy(1,:)); hold on;
+xlabel('Congruence');
+title('Individual Accuracy');
+
+result = [meanRT, meanAccuracy]
+%result = [accuracy, result];
+
